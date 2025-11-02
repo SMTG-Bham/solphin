9083a3b894a87ef0af971d553cabfdbfd953f977
Philippa U Cox <puc369@student.bahm.ac.uk>
2025-11-02 12:02:15 +0000

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='fom_placeholder',
+    version='0.1.0',    
+    description='Figure of Merit for Photovoltaics',
+    url='https://github.com/SMTG-Bham/PV-FoM',
+    author='Philippa U Cox, Peter P Russell',
+    author_email='puc369@student.bham.ac.uk, ppr466@student.bham.ac.uk',
+    license='MIT',
+    packages=['placeholder'],
+    install_requires=['numpy',
+                      'scipy',
+                      'logging',
+                      'matplotlib>=3.9.4',
+                      'pymatgen>=2024.8.9',
+                      'mpmath',
+                      'os',
+                      'shuntil',
+                      'pathlib',
+                      'typing_extensions',
+                      'monty'                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.9',
+    ],
+)
