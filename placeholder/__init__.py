0fa1acea408d4b4491860c802cce13b0c7d94f86
Philippa U Cox <puc369@student.bahm.ac.uk>
2025-11-02 12:11:47 +0000

    Fixed import notices

--- placeholder/__init__.py
+++ placeholder/__init__.py
@@ -1,6 +1,6 @@
-import pv_fom
-import db_fom
-import final_results
-import db_plotting
-import vasp_inputs
+import placeholder.pv_fom
+import placeholder.db_fom
+import placeholder.final_results
+import placeholder.db_plotting
+import placeholder.vasp_inputs
 from .version import __version__
