a7a902804454ee0b1dcbaa7c496c00dab43e519a
Philippa U Cox <puc369@student.bahm.ac.uk>
2025-11-21 11:42:16 +0000

    added spectral average and dispersion functionality

--- placeholder/__init__.py
+++ placeholder/__init__.py
@@ -3,5 +3,7 @@ import placeholder.db_fom
 import placeholder.final_results
 import placeholder.db_plotting
 import placeholder.vasp_inputs
+import placeholder.spectral
+import placeholder.optics
 from .version import __version__
 import placeholder.resources
