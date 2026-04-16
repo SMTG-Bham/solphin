71a3c7c26fe4023aba424527f3417f004fa526ca
pprussell <peterrussell102@gmail.com>
2026-04-16 16:31:59 +0100

    fixed merge

--- placeholder/__init__.py
+++
@@ -1,9 +0,0 @@
-import placeholder.pv_fom
-import placeholder.db_fom
-import placeholder.final_results
-import placeholder.db_plotting
-import placeholder.vasp_inputs
-import placeholder.spectral
-import placeholder.optics
-from .version import __version__
-import placeholder.resources
