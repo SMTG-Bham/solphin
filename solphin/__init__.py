7189f05825288eb0fe6f95fd5593fa85380ec774
Philippa U Cox <puc369@student.bham.ac.uk>
2026-05-11 16:20:12 +0100

    refactor to dos_eff_mass

--- solphin/__init__.py
+++ solphin/__init__.py
@@ -2,6 +2,7 @@ import solphin.pv_fom
 import solphin.db_fom
 import solphin.final_results
 import solphin.db_plots
+import solphin.dos_eff_mass
 import solphin.vasp_inputs
 import solphin.spectral
 import solphin.optics
