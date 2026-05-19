1e998e3b49dc667bc8cc84672c2233eec7c390d9
Philippa U Cox <puc369@student.bham.ac.uk>
2026-05-19 10:02:56 +0100

    fixed SQ limit inflationbug

--- solphin/db_fom.py
+++ solphin/db_fom.py
@@ -100,10 +100,10 @@ def convert_spectrum(spectrum):
     """
     converted = np.copy(spectrum)
     converted[:, 0] = converted[:, 0] * 1e-9  # wavelength to m
-    converted[:, 1] = converted[:, 1] * 1e-9  # irradiance to W/m2/m (from W/m2/nm)
+    converted[:, 1] = converted[:, 1] / 1e-9  # irradiance to W/m2/m (from W/m2/nm)
 
-    E = h * c / converted[:, 0]
-    d_lambda_d_E = h * c / E**2
+    E = h * c / converted[:, 0] # Bandgap in J
+    d_lambda_d_E = h * c / E**2 
     converted[:, 1] = converted[:, 1] * d_lambda_d_E * q / E
     converted[:, 0] = E / q
 
@@ -188,7 +188,7 @@ def current_density(E_gap, photon_spectrum, voltage, Tcell):
 
     """
 
-    return q * (photons_above_bandgap(E_gap, photon_spectrum) - rr0(E_gap, photon_spectrum, Tcell) * np.exp(q * voltage / (k * Tcell)) -1)
+    return q * (photons_above_bandgap(E_gap, photon_spectrum) - rr0(E_gap, photon_spectrum, Tcell) * np.exp(q * voltage / (k * Tcell))-1)
 
 
 def jsc(E_gap, photon_spectrum, Tcell):
@@ -226,7 +226,7 @@ def voc(E_gap, photon_spectrum, Tcell):
     J0 = rr0(E_gap, photon_spectrum, Tcell)
 
 
-    return (k * Tcell / q) * np.log( Jph / J0 + 1)
+    return (k * Tcell / q) * np.log( Jph / J0 +1)
 
 def v_at_mpp(E_gap, photon_spectrum):
 
