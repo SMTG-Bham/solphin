5474f928377f4daee0e3de10a08c007a3b35c83d
Louie <louies@hotmail.co.uk>
2026-08-21 11:25:37 +0100

    Clean up versioning  #20

--- solphin/__init__.py
+++ solphin/__init__.py
@@ -1,3 +1,5 @@
+from importlib.metadata import PackageNotFoundError, version
+
 import solphin.db_fom
 import solphin.db_plots
 import solphin.dos
@@ -6,4 +8,8 @@ import solphin.optics
 import solphin.pv_fom
 import solphin.spectral
 import solphin.vasp_inputs
-from .version import __version__
+
+try:  # single source of truth: the version declared in pyproject.toml
+    __version__ = version("solphin")
+except PackageNotFoundError:  # running from a source tree that was never installed
+    __version__ = "0.0.0"
