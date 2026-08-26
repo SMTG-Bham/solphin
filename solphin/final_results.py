2a03fec1d1b79b5c0dfc650208b2d868358bc100
Louie <louies@hotmail.co.uk>
2026-08-26 23:30:32 +0100

    Fixed mobility_plot bug  #66

--- solphin/final_results.py
+++ solphin/final_results.py
@@ -480,7 +480,7 @@ def mobility_plot(
         efficiency_list = []
 
         for mu in mobility_values:
-            efficiency = SQ_relative_FOM_PV_efficiency(
+            _, _, efficiency = SQ_relative_FOM_PV_efficiency(
                 E_gap,
                 photon_spectrum,
                 alpha,
